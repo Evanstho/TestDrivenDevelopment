c2ec9eec4092ee3bc0acf3e6172a14e644f002b5
T Evans <Evanstho@oregonstate.edu>
2024-02-20 17:06:32 -0800

    stopping development after adding "enough tests" without triggering a new failure

--- tests.py
+++ tests.py
@@ -35,10 +35,35 @@ class TestCase(unittest.TestCase):
     def test_8(self):
         input = "asd12!@A"
         self.assertTrue(check_pwd(input))
-    
+
     def test_8_orless(self):
         input = "1!aB"
         self.assertFalse(check_pwd(input))
+
+    def test_correct_password(self):
+        input = "as!@DF12a"
+        self.assertTrue(check_pwd(input))
+
+    def test_alldigit(self):
+        input = "1234567890"
+        self.assertFalse(check_pwd(input))
+
+    def test_alllower(self):
+        input = "asdfghjkl"
+        self.assertFalse(check_pwd(input))
+
+    def test_allupper(self):
+        input = "ASDFGHJKLASDFGHJKLASDFGGHSADKJ"
+        self.assertFalse(check_pwd(input))
+
+    def test_allsymbols(self):
+        input = "!@#$%^&*()!@"
+        self.assertFalse(check_pwd(input))
+
+    def test_over20_all(self):
+        input = "123asdASDgjhka!@#asdg+"
+        self.assertFalse(check_pwd(input))
+
 
 if __name__ == '__main__':
     unittest.main()

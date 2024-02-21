3d1d37de461d0cf421241be377cb0e45c2446981
T Evans <Evanstho@oregonstate.edu>
2024-02-20 16:31:05 -0800

    Updated check_pwd to return false when the password has no digits

--- check_pwd.py
+++ check_pwd.py
@@ -1,6 +1,13 @@
 def check_pwd(password):
+    hasdigit = False
 
     if not password:
+        return False
+    
+    for char in password:
+        if char.isdigit():
+            hasdigit = True
+    if not hasdigit:
         return False
 
     return True
